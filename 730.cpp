e01dbc8aa7e9fd7def91b18cf5b25cc8681f57b5
mnihank <88123741+mnihank@users.noreply.github.com>
2021-07-30 15:04:15 +0800

    Update 730.cpp

--- 730.cpp
+++ 730.cpp
@@ -7,13 +7,14 @@ screen.bgcolor('lightgreen')    # 設定螢幕背景
 myTurtle = turtle.Turtle()      # 產生一個小烏龜物件
 
 # turtle.Pen().screen.setworldcoordinates(0,0,500,400)  #設定螢幕寬高
-turtle.Pen().screen.setworldcoordinates(0,0,900,700)  #設定螢幕寬高
+turtle.Pen().screen.setworldcoordinates(0,0,900,900)  #設定螢幕寬高
 myTurtle.color('blue')          # 設定小烏龜顏色
-myTurtle.shape('turtle')        # 設定小烏龜形狀
+myTurtle.shape('classic')        # 設定小烏龜形狀
 myTurtle.left(90)               # 先將小烏龜方向調整向上
 
 myTurtle.penup()                # 提筆
 turtle.penup()                  # 畫筆提筆
+turtle.ht()                     #隱藏畫筆
 
 # myTurtle.setx(250) #設置機器人初始位置
 # myTurtle.sety(200) #設置機器人初始位置            
@@ -42,6 +43,22 @@ while 1:    #無限迴圈
     str(data)                   ##英文+數字
     if(data1>70):
         data1=200
+    elif(0<=data1<=10):
+        data1=5
+    elif(10<data1<=20):
+        data1=15
+    elif(20<data1<=30):
+        data1=25
+    elif(30<data1<=40):
+        data1=35
+    elif(40<data1<=50):
+        data1=45
+    elif(50<data1<=60):
+        data1=55
+    elif(60<data1<=70):
+        data1=65
+
+
     print(data)
     ##################################################掃地機器人位置 
     x,y=myTurtle.position()      #現在機器人位置
@@ -50,7 +67,14 @@ while 1:    #無限迴圈
                #LL   A
              ##方向  1:左方  2:右方
     if(data[0] == 'L'):
+        myTurtle.shapesize(0.5, 0.5, 1)
+        myTurtle.color('red')          # 設定小烏龜顏色
+        myTurtle.shape('square')        # 設定小烏龜形狀
         myTurtle.stamp()
+        myTurtle.resizemode("auto")
+        myTurtle.color('blue')          # 設定小烏龜顏色
+        myTurtle.shape('classic')        # 設定小烏龜形狀
+
         myTurtle.left(90)
         p=p-1
         lst=[]
@@ -63,7 +87,14 @@ while 1:    #無限迴圈
         u3=1
         j=2
     elif(data[0] == 'R'):
+        myTurtle.shapesize(0.5, 0.5, 1)
+        myTurtle.color('red')          # 設定小烏龜顏色
+        myTurtle.shape('square')        # 設定小烏龜形狀
         myTurtle.stamp()
+        myTurtle.resizemode("auto")
+        myTurtle.color('blue')          # 設定小烏龜顏色
+        myTurtle.shape('classic')        # 設定小烏龜形狀
+
         myTurtle.right(90)
         p=p+1
         lst=[]
@@ -76,7 +107,14 @@ while 1:    #無限迴圈
         u3=1
         j=1
     elif(data[0] == 'r'):
+        myTurtle.shapesize(0.5, 0.5, 1)
+        myTurtle.color('red')          # 設定小烏龜顏色
+        myTurtle.shape('square')        # 設定小烏龜形狀
         myTurtle.stamp()
+        myTurtle.resizemode("auto")
+        myTurtle.color('blue')          # 設定小烏龜顏色
+        myTurtle.shape('classic')        # 設定小烏龜形狀
+
         myTurtle.right(90)
         p=p+1
         lst=[]
@@ -89,7 +127,14 @@ while 1:    #無限迴圈
         u3=1
         j=2
     elif(data[0] == 'l'):
+        myTurtle.shapesize(0.5, 0.5, 1)
+        myTurtle.color('red')          # 設定小烏龜顏色
+        myTurtle.shape('square')        # 設定小烏龜形狀
         myTurtle.stamp()
+        myTurtle.resizemode("auto")
+        myTurtle.color('blue')          # 設定小烏龜顏色
+        myTurtle.shape('classic')        # 設定小烏龜形狀
+        
         myTurtle.left(90)
         p=p-1
         lst=[]
@@ -116,80 +161,48 @@ while 1:    #無限迴圈
                 lst.append(x1)
                 lst1.append(y1)
                 if(len(lst)>1):
-                    if(abs(lst[u]-lst[u1])>k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u1],lst1[u1])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
-                    elif(abs(lst[u]-lst[u1])<=k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u],lst1[u1])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
+                    turtle.setpos(lst[u],lst1[u])
+                    turtle.pendown()
+                    turtle.goto(lst[u1],lst1[u1])
+                    turtle.penup()
+                    u=u+1
+                    u1=u1+1
             elif(p==1):  #E
                 turtle.setpos(x, y+data1)
                 x1,y1=turtle.position()
                 lst.append(x1)
                 lst1.append(y1)
                 if(len(lst)>1):
-                    if(abs(lst1[u]-lst1[u1])>k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u1],lst1[u1])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
-                    elif(abs(lst1[u]-lst1[u1])<=k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u1],lst1[u])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
+                    turtle.setpos(lst[u],lst1[u])
+                    turtle.pendown()
+                    turtle.goto(lst[u1],lst1[u1])
+                    turtle.penup()
+                    u=u+1
+                    u1=u1+1
             elif(p==2):  #S
                 turtle.setpos(x+data1, y)
                 x1,y1=turtle.position()
                 lst.append(x1)
                 lst1.append(y1)
                 if(len(lst)>1):
-                    if(abs(lst[u]-lst[u1])>k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u1],lst1[u1])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
-                    elif(abs(lst[u]-lst[u1])<=k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u],lst1[u1])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
+                    turtle.setpos(lst[u],lst1[u])
+                    turtle.pendown()
+                    turtle.goto(lst[u1],lst1[u1])
+                    turtle.penup()
+                    u=u+1
+                    u1=u1+1
             elif(p==3):  #W
                 turtle.setpos(x, y-data1)
                 x1,y1=turtle.position()
                 lst.append(x1)
                 lst1.append(y1)
                 if(len(lst)>1):
-                    if(abs(lst1[u]-lst1[u1])>k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u1],lst1[u1])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
-                    elif(abs(lst1[u]-lst1[u1])<=k):
-                        turtle.setpos(lst[u],lst1[u])
-                        turtle.pendown()
-                        turtle.goto(lst[u1],lst1[u])
-                        turtle.penup()
-                        u=u+1
-                        u1=u1+1
+                    turtle.setpos(lst[u],lst1[u])
+                    turtle.pendown()
+                    turtle.goto(lst[u1],lst1[u1])
+                    turtle.penup()
+                    u=u+1
+                    u1=u1+1
     turtle.penup()
     if(j==2):    #TF方向朝右
         if(data1!=200):
@@ -199,80 +212,48 @@ while 1:    #無限迴圈
                 lst2.append(x1)
                 lst3.append(y1)
                 if(len(lst2)>1):
-                    if(abs(lst2[u2]-lst2[u3])>k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()
-                        turtle.goto(lst2[u3],lst3[u3])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
-                    elif(abs(lst2[u2]-lst2[u3])<=k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()
-                        turtle.goto(lst2[u2],lst3[u3])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
+                    turtle.setpos(lst2[u2],lst3[u2])
+                    turtle.pendown()
+                    turtle.goto(lst2[u3],lst3[u3])
+                    turtle.penup()
+                    u2=u2+1
+                    u3=u3+1
             elif(p==1):  #E
                 turtle.setpos(x, y-data1)
                 x1,y1=turtle.position()
                 lst2.append(x1)
                 lst3.append(y1)
                 if(len(lst2)>1):
-                    if(abs(lst3[u2]-lst3[u3])>k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()
-                        turtle.goto(lst2[u3],lst3[u3])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
-                    elif(abs(lst3[u2]-lst3[u3])<=k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()
-                        turtle.goto(lst2[u3],lst3[u2])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
+                    turtle.setpos(lst2[u2],lst3[u2])
+                    turtle.pendown()
+                    turtle.goto(lst2[u3],lst3[u3])
+                    turtle.penup()
+                    u2=u2+1
+                    u3=u3+1
             elif(p==2):  #S
                 turtle.setpos(x-data1, y)
                 x1,y1=turtle.position()
                 lst2.append(x1)
                 lst3.append(y1)
                 if(len(lst2)>1):
-                    if(abs(lst2[u2]-lst2[u3])>k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()
-                        turtle.goto(lst2[u3],lst3[u3])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
-                    elif(abs(lst2[u2]-lst2[u3])<=k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()
-                        turtle.goto(lst2[u2],lst3[u3])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
+                    turtle.setpos(lst2[u2],lst3[u2])
+                    turtle.pendown()
+                    turtle.goto(lst2[u3],lst3[u3])
+                    turtle.penup()
+                    u2=u2+1
+                    u3=u3+1
             elif(p==3):  #W
                 turtle.setpos(x, y+data1)
                 x1,y1=turtle.position()
                 lst2.append(x1)
                 lst3.append(y1)
                 if(len(lst2)>1):
-                    if(abs(lst3[u2]-lst3[u3])>k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()
-                        turtle.goto(lst2[u3],lst3[u3])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
-                    elif(abs(lst3[u2]-lst3[u3])<=k):
-                        turtle.setpos(lst2[u2],lst3[u2])
-                        turtle.pendown()           
-                        turtle.goto(lst2[u3],lst3[u2])
-                        turtle.penup()
-                        u2=u2+1
-                        u3=u3+1
+                    turtle.setpos(lst2[u2],lst3[u2])
+                    turtle.pendown()
+                    turtle.goto(lst2[u3],lst3[u3])
+                    turtle.penup()
+                    u2=u2+1
+                    u3=u3+1
 
 
         
